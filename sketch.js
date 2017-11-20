96a355ed91862b54bb549a331517fed2dc80caa8
Facundo Figueroa <facundofigueroa@Facundos-mbp.local>
2017-11-19 23:32:41 -0500

    Basic timer working.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+let minSelected;
+let startDate;
+
 function setup() {
 	noCanvas();
 }
 
 function draw() {
+	let currentDate = new Date().getTime();
+	let timeLeft = document.getElementById('timeLeft');
 
+
+
+	if (currentDate >= startDate) {
+		console.log('Timer Over');
+		timeLeft.innerHTML = "0";
+	} else {
+		timeLeft.innerHTML = ceil((startDate - currentDate) / 1000);
+	}
+}
+
+function getTime() {
+	let n = document.getElementById('numMinutes');
+	minSelected = n.value;
+	startDate = new Date();
+	startDate = (startDate.getTime() + minSelected*60000);
 }
